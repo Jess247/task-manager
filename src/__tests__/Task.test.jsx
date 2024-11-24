56e85eb7b5ba73dbe025b325e189ece7c478d7e9
Jessica <jesss8692@hotmail.com>
2024-11-24 17:57:10 +0100

    Add checkbox test

--- src/__tests__/Task.test.jsx
+++ src/__tests__/Task.test.jsx
@@ -4,13 +4,18 @@ import Task from '../components/Task';
 import Button from '../components/Button';
 
 describe('Render Task component', () => {
+    const task = {id:1, title:'Learn node.js', completed:true}
+
     test('renders a new task component with a title', () => {
-        const task = {id:1, title:'Learn node.js', completed:true}
-    
         render(<Task task={task}/>)
         expect(screen.getByText('Learn node.js')).toBeInTheDocument()
     })
 
+    test('checkbox reflects on completion status', () => {
+        render(<Task task={task}/>)
+        const checkbox = screen.getByRole('checkbox', {name:'completed'})
+        expect(checkbox).toBeChecked()
+    })
 
 })
 
@@ -21,7 +26,7 @@ describe('Render Button component', () => {
         render(<Button text={btnText} />)
         expect(screen.getByText('Click Me')).toBeInTheDocument()
     })
-    
+
     test('calls the onClick handler when clicked', () => {
         const handleClick = vi.fn()
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,6 +2,7 @@ function Task({task}) {
     return(
         <div>
             <p>{task.title}</p>
+            <Checkbox type="checkbox" name="checkbox" completed={task.completed}/>
         </div>
     )
 }
